0d118aa9548db94a407c50d9ca5397a96e337abd
Leonhardt Wille <leonhardt.wille@camunda.com>
2020-08-12 10:55:51 +0200

    feat(twitter): boilerplate Zeebe worker

--- packages/twitter/lib/twitter.ts
+++ packages/twitter/lib/twitter.ts
@@ -1,7 +1,27 @@
 'use strict';
 
-module.exports = twitter;
+const { ZBClient, Duration } = require('zeebe-node')
 
-function twitter() {
-    // TODO
+const zbc = new ZBClient({
+	onReady: () => console.log(`Connected!`),
+	onConnectionError: () => console.log(`Disconnected!`)
+})
+
+function handler(job, complete, worker) {
+	worker.log('Task variables', job.variables)
+
+	// Task worker business logic goes here
+	const updateToBrokerVariables = {
+		updatedProperty: 'newValue',
+	}
+
+	complete.success(updateToBrokerVariables)
 }
+
+const zbWorker = zbc.createWorker({
+    taskType: 'send-tweet',
+    taskHandler: handler,
+    onReady: () => console.log(`Worker connected!`),
+    onConnectionError: () => console.log(`Worker disconnected!`)
+})
+
