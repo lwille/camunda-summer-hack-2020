fcef7645dc7a89d83b4fce90107e946513f3d53a
Sebastian Bathke <sebastian.bathke@camunda.com>
2020-08-13 10:37:45 +0200

    message correlation

--- packages/twitter/lib/index.ts
+++ packages/twitter/lib/index.ts
@@ -2,6 +2,12 @@
 
 import { ZBClient } from "zeebe-node";
 
+if (!process.env.TWITTER_SEARCH_TERM) {
+  throw "Twitter search term environment variable (TWITTER_SEARCH_TERM) required!"
+}
+
+const searchTerm = process.env.TWITTER_SEARCH_TERM
+
 const zbc = new ZBClient({
   onReady: () => console.log(`Connected!`),
   onConnectionError: () => console.log(`Disconnected!`)
@@ -13,5 +19,12 @@ const zbc = new ZBClient({
   handlers.map((handler: any) => zbc.createWorker(handler.default));
 })();
 
+;(async () => {
+  const result = await zbc.createWorkflowInstance('lotteryProcess', {
+		lotteryTag: searchTerm,
+	})
+	console.log(result)
+})()
+
 import stream from './stream'
-stream(zbc);
+stream(zbc, searchTerm);
--- packages/twitter/lib/stream.ts
+++ packages/twitter/lib/stream.ts
@@ -4,15 +4,16 @@ import { Duration, ZBClient } from "zeebe-node";
 import T, { Tweet } from "./twitter-client";
 import {v4 as uuid} from "uuid";
 
-export default (zbc: ZBClient) => {
+export default (zbc: ZBClient, searchTerm: string) => {
   const stream = T.stream("statuses/filter", {
-    track: process.env.TWITTER_SEARCH_TERM || ""
+    track: searchTerm
   });
 
   stream.on("tweet", function(tweet: Tweet) {
     zbc.publishStartMessage({
       messageId: uuid(),
       name: "tweetFound",
+      correlationKey: searchTerm,
       variables: { tweet, tweetId: tweet.id_str, author: tweet.user.screen_name },
       timeToLive: Duration.seconds.of(10) // seconds
     });
